2319371ebab0766d2849e2d351fed7a202a915f2
Jonathan Rivers <jrivers@paradigm4.com>
2015-06-19 15:58:46 -0400

    Initial checkin of Hash functionswq

--- PhysicalGroupedAggregate.cpp
+++ PhysicalGroupedAggregate.cpp
@@ -15,6 +15,7 @@
 
 
 #include "query/Operator.h"
+#include "HashTableUtilities.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -49,8 +50,15 @@ public:
         shared_ptr<Array> inputArray = inputArrays[0];
         shared_ptr<ConstArrayIterator> arrayIter(inputArray->getConstIterator(0));  //everyone has an attribute 0! Even the strangest arrays...
         shared_ptr<ConstChunkIterator> chunkIter;
-        size_t numChunks =0;
-        size_t numCells  =0;
+        size_t numChunks = 0;
+        size_t numCells  = 0;
+
+        ArrayDesc const& inputDesc = inputArray->getArrayDesc();
+        //SpillingHashCollector collector(256*MB, this->_arena, inputDesc.getAttributes()[0], query);
+
+        AttributeComparator comparator(inputDesc.getAttributes()[0].getType());
+        MemoryHashTable memData(comparator, this->_arena);
+
         while (!arrayIter->end())
         {
             ++numChunks;
@@ -58,10 +66,42 @@ public:
             while(! chunkIter->end())
             {
                 ++numCells;
+                //collector.insert(chunkIter->getItem());
+                memData.insert(chunkIter->getItem());
                 ++(*chunkIter);
             }
             ++(*arrayIter);
         }
+
+        //shared_ptr<Array> outputArray =  collector.finalizeArray();
+
+
+        //shared_ptr<Array> outputArray(out);
+        //shared_ptr<ArrayIterator> outputArrayIter = outputArray->getIterator(0);
+        //Coordinates position(1, query->getInstanceID());
+        //shared_ptr<ChunkIterator> outputChunkIter = outputArrayIter->newChunk(position).getIterator(query, ChunkIterator::SEQUENTIAL_WRITE);
+
+
+        //shared_ptr<MemArray> redis = collector.makeExchangeArray(out,_schema);
+        //shared_ptr<ArrayIterator> outputArrayIter1 = out->getIterator(0);
+        //Coordinates position(1, query->getInstanceID());
+        //shared_ptr<ChunkIterator> outputChunkIter = outputArrayIter1->newChunk(position).getIterator(query, ChunkIterator::SEQUENTIAL_WRITE);
+        //outputChunkIter->setPosition(position);
+
+        /*
+        while(! outputChunkIter->end())
+        {
+        	//Value value;
+        	//value.setUint64(chunkIter->getItem());
+        	outputChunkIter->writeItem(chunkIter->getItem());
+        	outputChunkIter->flush();
+        	//chunkIter->getItem().
+        	//collector.insert(chunkIter->getItem());
+        	++(*chunkIter);
+        }
+        */
+
+
         shared_ptr<Array> outputArray(new MemArray(_schema, query));
         shared_ptr<ArrayIterator> outputArrayIter = outputArray->getIterator(0);
         Coordinates position(1, query->getInstanceID());
@@ -71,13 +111,31 @@ public:
         value.setUint64(numChunks);
         outputChunkIter->writeItem(value);
         outputChunkIter->flush();
+
         outputArrayIter = outputArray->getIterator(1);
         outputChunkIter = outputArrayIter->newChunk(position).getIterator(query, ChunkIterator::SEQUENTIAL_WRITE | ChunkIterator::NO_EMPTY_CHECK);
         outputChunkIter->setPosition(position);
         value.setUint64(numCells);
         outputChunkIter->writeItem(value);
         outputChunkIter->flush();
+
+        outputArrayIter = outputArray->getIterator(2);
+        outputChunkIter = outputArrayIter->newChunk(position).getIterator(query, ChunkIterator::SEQUENTIAL_WRITE | ChunkIterator::NO_EMPTY_CHECK);
+        outputChunkIter->setPosition(position);
+        //value.setUint64(collector.returnSize());
+        value.setUint64(memData.size());
+        outputChunkIter->writeItem(value);
+        outputChunkIter->flush();
+
+
+        //shared_ptr<MemArray> outfoo = collector.finalize();
+        //outfoo.reset();
+
         return outputArray;
+
+       //return out;
+       //return outputArray;
+
     }
 };
 REGISTER_PHYSICAL_OPERATOR_FACTORY(PhysicalGroupedAggregate, "grouped_aggregate", "physical_grouped_aggregate");
