26a6d99ddbdb536a54d3e9c47a9e2bcade9ba01c
apoliakov <apoliakov@paradigm4.com>
2015-11-30 18:04:30 -0500

    Onwards

--- PhysicalGroupedAggregate.cpp
+++ PhysicalGroupedAggregate.cpp
@@ -41,7 +41,174 @@ using namespace std;
 namespace scidb
 {
 
+namespace grouped_aggregate
+{
+
+class StateWriter : public boost::noncopyable
+{
+
+private:
+    shared_ptr<Array> const _output;
+    Coordinates _outputPosition;
+    size_t const _numAttributes;
+    vector<shared_ptr<ArrayIterator> > _outputArrayIterators;
+    vector<shared_ptr<ChunkIterator> > _outputChunkIterators;
+    Value _buf;
+
+public:
+    static ArrayDesc makeSchema(TypeId const& attributeType, size_t const chunkSize)
+    {
+        Attributes outputAttributes;
+        outputAttributes.push_back( AttributeDesc(0, "bucket", TID_UINT64,    0, 0));
+        outputAttributes.push_back( AttributeDesc(1, "hash",   TID_UINT64,    0, 0));
+        outputAttributes.push_back( AttributeDesc(2, "value",  attributeType, 0, 0));
+        outputAttributes = addEmptyTagAttribute(outputAttributes);
+        Dimensions outputDimensions;
+        outputDimensions.push_back(DimensionDesc("i", 0, CoordinateBounds::getMax(), chunkSize, 0));
+        return ArrayDesc("grouped_aggregate_state", outputAttributes, outputDimensions, defaultPartitioning());
+    }
+
+    StateWriter(TypeId const& attributeType, size_t const chunkSize):
+        _output(make_shared<MemArray>(schema,query)),
+        _outputPosition( splitOnDimension ? 5 : 4, 0),
+        _numLiveAttributes(schema.getAttributes(true).size()),
+        _outputLineSize(splitOnDimension ? schema.getDimensions()[4].getChunkInterval() : _numLiveAttributes),
+        _outputChunkSize(schema.getDimensions()[3].getChunkInterval()),
+        _outputArrayIterators(_numLiveAttributes),
+        _outputChunkIterators(_numLiveAttributes),
+        _splitOnDimension(splitOnDimension),
+        _outputColumn(0),
+        _attributeDelimiter(attDelimiter)
+    {
+        for(AttributeID i =0; i<_numLiveAttributes; ++i)
+        {
+            _outputArrayIterators[i] = _output->getIterator(i);
+        }
+    }
+
+    void newChunk (Coordinates const& inputChunkPosition, shared_ptr<Query>& query)
+    {
+        _outputPosition[0] = inputChunkPosition[0];
+        _outputPosition[1] = inputChunkPosition[1];
+        _outputPosition[2] = inputChunkPosition[2];
+        _outputPosition[3] = 0;
+        if(_splitOnDimension)
+        {
+            _outputPosition[4] = 0;
+        }
+        for(AttributeID i =0; i<_numLiveAttributes; ++i)
+        {
+            if(_outputChunkIterators[i].get())
+            {
+                _outputChunkIterators[i]->flush();
+            }
+            _outputChunkIterators[i] = _outputArrayIterators[i]->newChunk(_outputPosition).getIterator(query,
+                i == 0 ? ChunkIterator::SEQUENTIAL_WRITE : ChunkIterator::SEQUENTIAL_WRITE | ChunkIterator::NO_EMPTY_CHECK);
+        }
+    }
+
+    void writeValue (char const* start, char const* end)
+    {
+        if(_outputColumn < _outputLineSize - 1)
+        {
+            _buf.setSize(end - start + 1);
+            char* d = _buf.getData<char>();
+            memcpy(d, start, end-start);
+            d[(end-start)]=0;
+            if(_splitOnDimension)
+            {
+                _outputChunkIterators[0] -> setPosition(_outputPosition);
+                _outputChunkIterators[0] -> writeItem(_buf);
+                ++(_outputPosition[4]);
+            }
+            else
+            {
+                _outputChunkIterators[_outputColumn] -> setPosition(_outputPosition);
+                _outputChunkIterators[_outputColumn] -> writeItem(_buf);
+            }
+        }
+        else if (_outputColumn == _outputLineSize - 1)
+        {
+            string value(start, end-start);
+            _errorBuf << "long" << _attributeDelimiter << value;
+        }
+        else
+        {
+            string value(start, end-start);
+            _errorBuf << _attributeDelimiter << value;
+        }
+        ++_outputColumn;
+    }
+
+    void endLine()
+    {
+        if(_outputColumn < _outputLineSize - 1)
+        {
+            _buf.setNull();
+            if(_splitOnDimension)
+            {
+                while (_outputColumn < _outputLineSize - 1)
+                {
+                    _outputChunkIterators[0] -> setPosition(_outputPosition);
+                    _outputChunkIterators[0] -> writeItem(_buf);
+                    ++(_outputPosition[4]);
+                    ++_outputColumn;
+                }
+            }
+            else
+            {
+                while (_outputColumn < _outputLineSize - 1)
+                {
+                    _outputChunkIterators[_outputColumn] -> setPosition(_outputPosition);
+                    _outputChunkIterators[_outputColumn] -> writeItem(_buf);
+                    ++_outputColumn;
+                }
+            }
+            _errorBuf << "short";
+        }
+        if(_errorBuf.str().size())
+        {
+            _buf.setString(_errorBuf.str());
+        }
+        else
+        {
+            _buf.setNull();
+        }
+        if(_splitOnDimension)
+        {
+            _outputChunkIterators[0] -> setPosition(_outputPosition);
+            _outputChunkIterators[0] -> writeItem(_buf);
+            _outputPosition[4] = 0;
+        }
+        else
+        {
+            _outputChunkIterators[_outputLineSize - 1] -> setPosition(_outputPosition);
+            _outputChunkIterators[_outputLineSize - 1] -> writeItem(_buf);
+        }
+        ++(_outputPosition[3]);
+        _errorBuf.str("");
+        _outputColumn = 0;
+    }
+
+    shared_ptr<Array> finalize()
+    {
+        for(AttributeID i =0; i<_numLiveAttributes; ++i)
+        {
+            if(_outputChunkIterators[i].get())
+            {
+                _outputChunkIterators[i]->flush();
+            }
+            _outputChunkIterators[i].reset();
+            _outputArrayIterators[i].reset();
+        }
+        return _output;
+    }
+};
+
+} //namespace grouped_aggregate
+
 using namespace std;
+using namespace grouped_aggregate
 using std::shared_ptr;
 
 class PhysicalGroupedAggregate : public PhysicalOperator
