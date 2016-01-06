0ad637abe5decf4c85ba6682faa5bdd49ddb9495
apoliakov <apoliakov@paradigm4.com>
2016-01-05 19:18:32 -0500

    Fix up some headers

--- AggregateHashTable.h
+++ AggregateHashTable.h
@@ -1,3 +1,18 @@
+/*
+**
+* BEGIN_COPYRIGHT
+*
+* PARADIGM4 INC.
+* This file is part of the Paradigm4 Enterprise SciDB distribution kit
+* and may only be used with a valid Paradigm4 contract and in accord
+* with the terms and conditions specified by that contract.
+*
+* Copyright (C) 2010 - 2015 Paradigm4 Inc.
+* All Rights Reserved.
+*
+* END_COPYRIGHT
+*/
+
 #ifndef HASH_TABLE_UTILITIES
 #define HASH_TABLE_UTILITIES
 
